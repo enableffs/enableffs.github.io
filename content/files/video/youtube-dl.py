ca97988de7c6ce6abbbcb9498a5d69ccf6d020aa
munash48 <mugwanyan@gmail.com>
2019-02-20 10:32:56 +0300

    Key words replaced

--- content/files/video/youtube-dl.py
+++ content/files/video/youtube-dl.py
@@ -33,16 +33,16 @@ languages = ['en', 'sw', 'fr']
 
 videos.append(Video("YZlHgb0n2b4", "vision"))       # Disability is not inability
 videos.append(Video("chJcfaP12Zc", "vision"))       # Blind woman make baskets
-videos.append(Video("owxORyUdYec", "vision"))       # A blind taylor trains students with visual impairment
-videos.append(Video("9hLupgYcHqU", "vision"))       # Blind students learn to use computers
-videos.append(Video("abapaB5Dvgk", "dual"))         # Deafblind students at Kilimani Deafblind Unit in Nairobi
+videos.append(Video("owxORyUdYec", "vision"))       # A blind taylor trains  learners with visual impairment
+videos.append(Video("9hLupgYcHqU", "vision"))       # Blind  learners learn to use computers
+videos.append(Video("abapaB5Dvgk", "dual"))         # Deafblind  learners at Kilimani Deafblind Unit in Nairobi
 videos.append(Video("EhvIfOFSXCQ", "dual"))         # Joy of life
 videos.append(Video("lofZBB7wNso", "dual"))         # Using tactile senses
 videos.append(Video("sT2OagpoOhg", "hearing"))      # Vocational training for deaf in secondary school
 videos.append(Video("-VoZZJZrhDY", "hearing"))      # Interpreters learning sign language
-videos.append(Video("viFMjSzrI6o", "hearing"))      # Information about sexual issues for deaf students
+videos.append(Video("viFMjSzrI6o", "hearing"))      # Information about sexual issues for deaf  learners
 videos.append(Video("2OI3-qMk_1I", "hearing"))      # Deaf children make sandwiches
-videos.append(Video("XcyaN7ayLkE", "hearing"))      # Vocational training deaf students
+videos.append(Video("XcyaN7ayLkE", "hearing"))      # Vocational training deaf  learners
 videos.append(Video("Aney9RqlvIs", "hearing"))      # Vocational training deaf women
 videos.append(Video("UwRZgrTeT74", "hearing"))      # Deaf preschool children learning sign through physical activity
 videos.append(Video("sEFig4BJOI8", "vision"))       # Blind children learn to type on Perkins braille-writer
@@ -50,8 +50,8 @@ videos.append(Video("TZZOvuGZ17w", "vision"))       # From written text to Brail
 videos.append(Video("65_pQQpnI24", "vision"))       # Inclusion at Bishop Willis Demonstration School in Iganga, Uganda
 videos.append(Video("5S8MYNvfeWc", "vision"))       # Blind student moving around at the campus with white cane
 videos.append(Video("GkaNi4MIPuQ", "vision"))       # Use of concrete materials in the learning process
-videos.append(Video("1W-B0rqiEGk", "vision"))       # Blind students without white canes
-videos.append(Video("3E7Sam17rTs", "vision"))       # Blind students moving around
+videos.append(Video("1W-B0rqiEGk", "vision"))       # Blind  learners without white canes
+videos.append(Video("3E7Sam17rTs", "vision"))       # Blind  learners moving around
 
 
 ######################################################################################################
